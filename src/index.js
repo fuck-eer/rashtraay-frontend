da71340136ab2eeeb7a7cbbee2d5c592c8920998
fuck-eer <nishchaychauhan8@gmail.com>
2020-09-15 01:24:18 +0530

    redux tools managed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+cconst composeEnhancers = process.env.NODE_ENV==='development'?
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__:null || compose;
 
 const rootReducer=combineReducers({
   memlog:memlogreducer
